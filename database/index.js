60039830bd7f047da65cbe64b5c3f3f2fd695897
Andrew Dibble <acdibble@gmail.com>
2020-03-26 23:38:25 +0100

    Update dependencies

--- database/index.js
+++ database/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
-const Promise = require('bluebird');
 
 const db = new sqlite3.Database(path.join(__dirname, '..', 'todos.db'), (err) => {
   if (err) {
